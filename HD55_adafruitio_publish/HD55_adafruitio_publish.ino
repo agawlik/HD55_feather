e94220e2a2adfe82ddabc191e433c50310aa1d34
agawlik <agawlik@users.noreply.github.com>
2024-09-24 22:44:38 -0400

    cleanup

--- HD55_adafruitio_publish/HD55_adafruitio_publish.ino
+++ HD55_adafruitio_publish/HD55_adafruitio_publish.ino
@@ -1,4 +1,3 @@
-
 // Adafruit IO Publish Example
 //
 // Adafruit invests time and resources providing this open source code.
@@ -13,23 +12,32 @@
 
 /************************** Configuration ***********************************/
 
-// edit the config.h tab and enter your Adafruit IO credentials
+// edit the private_config.h tab and enter your Adafruit IO credentials
 // and any additional configuration needed for WiFi, cellular,
 // or ethernet clients.
 #include "private_config.h"
 
-#define CS_PIN 5
+//! MCP2515 CanBus FeatherWing CS
+#define MCP2515_CS_PIN (5) 
+//! 125kbps CAN baudrate
 #define CAN_BAUDRATE (125000)
+//! polling period [seconds]
 #define POLL_DELAY (60000)
+
+//! Force wait for serial monitor before initializing
+//#define WAIT_FOR_SERIAL_MONITOR (1)
+
+//! Serial monitor baudrate
+#define SERIAL_MONITOR_BAUD (115200)
 
 /************************ Example Starts Here *******************************/
 
 #include <string.h>
 #include <Adafruit_MCP2515.h>
 
-Adafruit_MCP2515 mcp(CS_PIN);
+Adafruit_MCP2515 mcp(MCP2515_CS_PIN); //!< CanBus FeatherWing
 
-// set up the 'counter' feed
+// set up the adafruit IO feeds
 AdafruitIO_Feed *io_binary = io.feed("hd55-1.hd55-binary");
 AdafruitIO_Feed *io_rh = io.feed("hd55-1.relative-humidity");
 AdafruitIO_Feed *io_temperature = io.feed("hd55-1.temperature");
@@ -39,11 +47,13 @@ AdafruitIO_Feed *io_status = io.feed("hd55-1.status");
 void setup() {
 
   // start the serial connection
-  Serial.begin(115200);
+  Serial.begin(SERIAL_MONITOR_BAUD);
 
+#ifdef WAIT_FOR_SERIAL_MONITOR
   // wait for serial monitor to open
-  // while (!Serial)
-  //   ;
+  while (!Serial)
+    ;
+#endif
 
   Serial.print("Connecting to Adafruit IO");
 
@@ -101,7 +111,6 @@ void loop() {
       Serial.println("timeout");
       return;
     }
-
   }
 
   Serial.print("Received ");
@@ -131,22 +140,28 @@ void loop() {
       snprintf(&packet_data[i * 2], 3, "%02X", read_data);
       Serial.print(read_data, HEX);
       Serial.print(" ");
+      switch (read_data)
+      {
+        case 0: // RH_actual
+          io_rh->save(read_data);
+          break;
+        case 1: // RH_setpoint
+          io_setpoint->save(read_data);
+          break;
+        case 2: // Temperature
+          io_temperature->save(read_data);
+          break;
+        case 4: // Status
+          io_status->save(read_data);
+          break;
+        default:
+          break;
+      }
     }
     Serial.println();
   }
 #else
   memcpy(packet_data, "3032160003000000", 16);
-#endif
-
-
-
-  // save count to the 'counter' feed on Adafruit IO
-  Serial.print("sending -> ");
-  Serial.write(packet_data,16);
-  Serial.println();
-
-  io_binary->save(packet_data);
-
   char value[3] = {0};
   memcpy(value, &packet_data[0], 2);
   io_rh->save(value);
@@ -156,11 +171,19 @@ void loop() {
   io_temperature->save(value);
   memcpy(value, &packet_data[8], 2);
   io_status->save(value);
+#endif
+
+  // save count to the feed on Adafruit IO
+  Serial.print("sending -> ");
+  Serial.write(packet_data,16);
+  Serial.println();
+
+  io_binary->save(packet_data);
+
 
   Serial.println();
 
   // Adafruit IO is rate limited for publishing, so a delay is required in
-  // between feed->save events. In this example, we will wait three seconds
-  // (1000 milliseconds == 1 second) during each loop.
+  // between feed->save events.
   delay(POLL_DELAY);
 }
