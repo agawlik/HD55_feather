7140582129fc8814dbc4cc2a7fb86b5754105fca
agawlik <agawlik@users.noreply.github.com>
2024-09-24 22:23:22 -0400

    initial with working HD55_adafruitio_publish

---
+++ HD55_adafruitio_publish/HD55_adafruitio_publish.ino
@@ -0,0 +1,166 @@
+
+// Adafruit IO Publish Example
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "private_config.h"
+
+#define CS_PIN 5
+#define CAN_BAUDRATE (125000)
+#define POLL_DELAY (60000)
+
+/************************ Example Starts Here *******************************/
+
+#include <string.h>
+#include <Adafruit_MCP2515.h>
+
+Adafruit_MCP2515 mcp(CS_PIN);
+
+// set up the 'counter' feed
+AdafruitIO_Feed *io_binary = io.feed("hd55-1.hd55-binary");
+AdafruitIO_Feed *io_rh = io.feed("hd55-1.relative-humidity");
+AdafruitIO_Feed *io_temperature = io.feed("hd55-1.temperature");
+AdafruitIO_Feed *io_setpoint = io.feed("hd55-1.setpoint");
+AdafruitIO_Feed *io_status = io.feed("hd55-1.status");
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  // while (!Serial)
+  //   ;
+
+  Serial.print("Connecting to Adafruit IO");
+
+  // connect to io.adafruit.com
+  io.connect();
+
+  // wait for a connection
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+  if (!mcp.begin(CAN_BAUDRATE)) {
+    Serial.println("Error initializing MCP2515.");
+    while (1) delay(10);
+  }
+  Serial.println("MCP2515 chip found");
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  char packet_data[17] = {0};
+#if 1
+  // send packet: id is 11 bits, packet can contain up to 8 bytes of data
+  Serial.print("Sending packet ... ");
+  mcp.beginPacket(0x123);
+  mcp.write(0x01);  // always one
+  mcp.write(0x00);  // no button pressed
+  mcp.write(0x00);  // 0x00 (except when button pressed, see above)
+  mcp.write(50);    // relative humidity
+  mcp.write(18);    // temperature [C]
+  mcp.write(0x00);
+  mcp.write(0x00);
+  mcp.write(0x00);
+  mcp.endPacket();
+  Serial.println("done");
+
+  Serial.print("Receiving packet ... ");
+  int packetSize = 0;
+  auto start_time = millis();
+  while (packetSize == 0) {
+    packetSize = mcp.parsePacket();
+    if (millis() - start_time > POLL_DELAY)
+    {
+      Serial.println("timeout");
+      return;
+    }
+
+  }
+
+  Serial.print("Received ");
+
+  if (mcp.packetExtended()) {
+    Serial.print("extended ");
+  }
+
+  if (mcp.packetRtr()) {
+    // Remote transmission request, packet contains no data
+    Serial.print("RTR ");
+  }
+
+  Serial.print("packet with id 0x");
+  Serial.print(mcp.packetId(), HEX);
+
+  if (mcp.packetRtr()) {
+    Serial.print(" and requested length ");
+    Serial.println(mcp.packetDlc());
+  } else {
+    Serial.print(" and length ");
+    Serial.println(packetSize);
+
+    // only print packet data for non-RTR packets
+    for (int i = 0; i < 8; i++) {
+      int read_data = mcp.read();
+      snprintf(&packet_data[i * 2], 3, "%02X", read_data);
+      Serial.print(read_data, HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+#else
+  memcpy(packet_data, "3032160003000000", 16);
+#endif
+
+
+
+  // save count to the 'counter' feed on Adafruit IO
+  Serial.print("sending -> ");
+  Serial.write(packet_data,16);
+  Serial.println();
+
+  io_binary->save(packet_data);
+
+  char value[3] = {0};
+  memcpy(value, &packet_data[0], 2);
+  io_rh->save(value);
+  memcpy(value, &packet_data[2], 2);
+  io_setpoint->save(value);
+  memcpy(value, &packet_data[4], 2);
+  io_temperature->save(value);
+  memcpy(value, &packet_data[8], 2);
+  io_status->save(value);
+
+  Serial.println();
+
+  // Adafruit IO is rate limited for publishing, so a delay is required in
+  // between feed->save events. In this example, we will wait three seconds
+  // (1000 milliseconds == 1 second) during each loop.
+  delay(POLL_DELAY);
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # HD55_feather
-AlorAir HD55 interface from Feather M0 Wifi module
+AlorAir HD55 interface from Feather M0 Wifi module and MCP2515 feather
+
+# Hardware
+* [Adafruit Feather M0 WiFi - ATSAMD21 + ATWINC1500](https://www.adafruit.com/product/3010)
+* [Adafruit CAN Bus FeatherWing - MCP2515](https://www.adafruit.com/product/5709)
+
+![Adafruit Feather M0 WiFi - ATSAMD21 + ATWINC1500](https://cdn-learn.adafruit.com/assets/assets/000/110/927/original/adafruit_products_Adafruit_Feather_M0_WINC1500_Pinout.png?1650391656)
+
+See [Feather M0 Wifi Arduino setup](https://learn.adafruit.com/adafruit-feather-m0-wifi-atwinc1500/setup). Note that WIFI firmware update must be performed with Arduino IDE v1.x.
+
+| Pin | Use |
+| --- | --- |
+| 2 | WIFI ENA | 
+| 4 | WIFI RST | 
+| 5 | MCP2515 CS | 
+| 7 | WIFI IRQ | 
+| 8 | WIFI CS | 
+| 13 | ID - #1) GND, #2) pull-up high | 
+| 22 | MISO | 
+| 23 | MOSI | 
+| 24 | SCK |
+
+## HD55 CanBus Protocol
+My HD55 units are configured for 125kbps and have the 0x123 response format described in [this forum post](https://community.home-assistant.io/t/automate-alorair-sentinel-hd55-dehumidifier/306084/22). An older, more rigorous description is available at [tinymicros wiki](https://tinymicros.com/wiki/AlorAir_Sentinel_HD55_Dehumidifier).
+
+```
+0x123 RH_actual RH_set Temp D3 Status D5 D6 D7
+
+Temp is in celsius
+Statusbit1 = 1 → Unit is running
+Statusbit0 = 1 → Unit is on
+Statusbit0 = 0 → Unit is off
+(bit0 is Least Significant Bit)
+```
+
+An example of a received packet while the system is running (all in hex): `30 32 16 0 3 0 0 0` where setpoint is RH=50% (0x32).
+
+# Sketches
+All sketches include a `private_config.h` file that contains Adafruit IO key and wifi network SSID and password. This private configuration template can be copied from the `adafruitio_00_publish` example sketch.
+
+Note that all sketches using the Feather M0 WiFi require the following:
+```
+//Configure pins for Adafruit ATWINC1500 Feather
+WiFi.setPins(8,7,4,2);
+```
+
+## HD55_adafruitio_publish
+This is an adaption of the adafruitio_00_publish sketch and MCP2515 example to request telemetry from the HD55 and then publish the data to a number of Adafruit IO feeds every 1 minute.
+
